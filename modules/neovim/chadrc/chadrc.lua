6542a5b1937bdf4f6176583a14ef63bf54591ec9
Skylar Abruzese <skylar@abruzese.com>
2025-10-10 16:20:01 -0400

    oops added chadrc

---
+++ modules/neovim/chadrc/chadrc.lua
@@ -0,0 +1,52 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+---@type ChadrcConfig
+local M = {}
+M.base46 = {
+    theme = "catppuccin",
+    transparency = true, -- Enable transparency
+    -- hl_override = {
+    --     Comment = { italic = true },
+    --     ["@comment"] = { italic = true },
+    -- },
+}
+M.nvdash = {
+    load_on_startup = true,
+    header = {
+        "                            ",
+        "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+        "   ▄▀███▄     ▄██ █████▀    ",
+        "   ██▄▀███▄   ███           ",
+        "   ███  ▀███▄ ███           ",
+        "   ███    ▀██ ███           ",
+        "   ███      ▀ ███           ",
+        "   ▀██ █████▄▀█▀▄██████▄    ",
+        "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+        "                            ",
+        "     Powered By  eovim    ",
+        "                            ",
+    },
+    buttons = {
+        { txt = "  Open Oil", keys = "o", cmd = "Oil" },
+        { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+        { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+        { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+        { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+        { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+        { txt = "  Configuration", keys = "cf", cmd = ":lua require('telescope.builtin').find_files({cwd = vim.fn.stdpath('config')}) <CR>" },
+        { txt = "󰈆  Quit Neovim", keys = "q", cmd = ":qa<CR>" },
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+        {
+            txt = function()
+                local stats = require("lazy").stats()
+                local ms = math.floor(stats.startuptime) .. " ms"
+                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+            end,
+            hl = "NvDashFooter",
+            no_gap = true,
+        },
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    }
+}
+return M
