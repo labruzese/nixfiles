34d89a583942c0929af1544468430f873f3114ab
Skylar Abruzese <skylar@abruzese.com>
2025-10-20 14:46:40 -0400

    neovim -- disable nvdash on startup

--- modules/neovim/chadrc/chadrc.lua
+++ modules/neovim/chadrc/chadrc.lua
@@ -4,49 +4,49 @@
 ---@type ChadrcConfig
 local M = {}
 M.base46 = {
-    theme = "catppuccin",
-    transparency = true, -- Enable transparency
-    -- hl_override = {
-    --     Comment = { italic = true },
-    --     ["@comment"] = { italic = true },
-    -- },
+	theme = "catppuccin",
+	transparency = true, -- Enable transparency
+	-- hl_override = {
+	--     Comment = { italic = true },
+	--     ["@comment"] = { italic = true },
+	-- },
 }
 M.nvdash = {
-    load_on_startup = true,
-    header = {
-        "                            ",
-        "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
-        "   ▄▀███▄     ▄██ █████▀    ",
-        "   ██▄▀███▄   ███           ",
-        "   ███  ▀███▄ ███           ",
-        "   ███    ▀██ ███           ",
-        "   ███      ▀ ███           ",
-        "   ▀██ █████▄▀█▀▄██████▄    ",
-        "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
-        "                            ",
-        "     Powered By  eovim    ",
-        "                            ",
-    },
-    buttons = {
-        { txt = "  Open Oil", keys = "o", cmd = "Oil" },
-        { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
-        { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
-        { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
-        { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
-        { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
-        { txt = "  Configuration", keys = "cf", cmd = ":lua require('telescope.builtin').find_files({cwd = vim.fn.stdpath('config')}) <CR>" },
-        { txt = "󰈆  Quit Neovim", keys = "q", cmd = ":qa<CR>" },
-        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-        {
-            txt = function()
-                local stats = require("lazy").stats()
-                local ms = math.floor(stats.startuptime) .. " ms"
-                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-            end,
-            hl = "NvDashFooter",
-            no_gap = true,
-        },
-        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-    }
+	load_on_startup = false,
+	header = {
+		"                            ",
+		"     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+		"   ▄▀███▄     ▄██ █████▀    ",
+		"   ██▄▀███▄   ███           ",
+		"   ███  ▀███▄ ███           ",
+		"   ███    ▀██ ███           ",
+		"   ███      ▀ ███           ",
+		"   ▀██ █████▄▀█▀▄██████▄    ",
+		"     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+		"                            ",
+		"     Powered By  eovim    ",
+		"                            ",
+	},
+	buttons = {
+		{ txt = "  Open Oil", keys = "o", cmd = "Oil" },
+		{ txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+		{ txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+		{ txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+		{ txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+		{ txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+		{ txt = "  Configuration", keys = "cf", cmd = ":lua require('telescope.builtin').find_files({cwd = vim.fn.stdpath('config')}) <CR>" },
+		{ txt = "󰈆  Quit Neovim", keys = "q", cmd = ":qa<CR>" },
+		{ txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+		{
+			txt = function()
+				local stats = require("lazy").stats()
+				local ms = math.floor(stats.startuptime) .. " ms"
+				return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+			end,
+			hl = "NvDashFooter",
+			no_gap = true,
+		},
+		{ txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+	}
 }
 return M
