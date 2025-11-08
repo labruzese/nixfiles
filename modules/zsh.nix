17a51adb0a32d8e90fbfdfdfcf4f8d6b3299cc19
Skylar Abruzese <skylar@abruzese.com>
2025-11-07 22:04:42 -0500

    fixing more zsh stuff

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -11,7 +11,6 @@
 
     history = {
       size = 10000;
-      path = "${config.xdg.dataHome}/zsh/zsh_history";
     };
 
     # Vi mode settings
@@ -36,43 +35,21 @@
       ];
 
       extraConfig = ''
-        # Vi mode configuration
-        KEYTIMEOUT=15
-        VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
-        MODE_INDICATOR="%F{blue} %f"
-        INSERT_MODE_INDICATOR="%F{green} %f"
-        VI_MODE_CURSOR_VISUAL=0
+        	# Vi mode configuration
+            KEYTIMEOUT=15
+            VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
+            MODE_INDICATOR="%F{blue} %f"
+            INSERT_MODE_INDICATOR="%F{green} %f"
+            VI_MODE_CURSOR_VISUAL=0
 
-        HYPHEN_INSENSITIVE="true"
+            HYPHEN_INSENSITIVE="true"
       '';
     };
 
     # mostly enviornments 
     initContent = ''
-      # Add custom paths to PATH
-      export PATH="$HOME/scripts:$HOME/.cargo/bin:$PATH"
-      
-      # Add vi mode indicator to prompt
-      PROMPT="$(vi_mode_prompt_info)$PROMPT"
-      
-      # WezTerm shell integration
-      if [[ "$TERM_PROGRAM" == "WezTerm" ]]; then
-        source ~/.config/wezterm/shell-integration/wezterm.sh
-      fi
-      
-      # Pyenv initialization
-      export PYENV_ROOT="$HOME/.pyenv"
-      [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-      eval "$(pyenv init -)"
-      
-      # GHCup environment
-      [ -f "/home/sky/.ghcup/env" ] && . "/home/sky/.ghcup/env"
-      
-      # Zoxide initialization
-      eval "$(zoxide init --cmd cd zsh)"
-      
-      # Opam configuration
-      [[ ! -r '/home/sky/.opam/opam-init/init.zsh' ]] || source '/home/sky/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+      	    # Add custom paths to PATH
+      	    export PATH="$HOME/scripts:$HOME/.cargo/bin:$PATH"
     '';
 
     shellAliases = {
@@ -80,6 +57,32 @@
       trash = "gio trash";
       copy = "wl-copy";
     };
+
+    initExtra = ''
+              			
+      	# Add vi mode indicator to prompt
+        	PROMPT="$(vi_mode_prompt_info)$PROMPT"
+
+        	# WezTerm shell integration
+        	if [[ "$TERM_PROGRAM" == "WezTerm" ]]; then
+        	  source ~/.config/wezterm/shell-integration/wezterm.sh
+        	fi
+
+        	# Pyenv initialization
+        	export PYENV_ROOT="$HOME/.pyenv"
+        	[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+        	eval "$(pyenv init -)"
+
+        	# GHCup environment
+        	[ -f "/home/sky/.ghcup/env" ] && . "/home/sky/.ghcup/env"
+
+        	# Zoxide initialization
+        	eval "$(zoxide init --cmd cd zsh)"
+
+        	# Opam configuration
+        	[[ ! -r '/home/sky/.opam/opam-init/init.zsh' ]] || source '/home/sky/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+        	'';
+
 
   };
 
