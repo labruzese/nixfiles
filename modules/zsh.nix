e0ea8854ef3de5a5e01126aae962ac465512ea13
Skylar Abruzese <skylar@abruzese.com>
2025-11-11 13:20:48 -0500

    update vi-mode indicator to update properly

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -41,51 +41,51 @@
       ];
 
       extraConfig = ''
-        			# Vi mode configuration
-        			VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
-        			KEYTIMEOUT=15
-        			MODE_INDICATOR="%F{blue} %f"
-        			INSERT_MODE_INDICATOR="%F{green} %f"
-        			VI_MODE_CURSOR_VISUAL=0
+        # Vi mode configuration
+        VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
+        KEYTIMEOUT=15
+        MODE_INDICATOR="%F{blue} %f"
+        INSERT_MODE_INDICATOR="%F{green} %f"
+        VI_MODE_CURSOR_VISUAL=0
 
-        			HYPHEN_INSENSITIVE="true"
+        HYPHEN_INSENSITIVE="true"
       '';
     };
 
     initContent =
       let
         enviornments = lib.mkBefore ''
-          			# path
-          			export PATH="$HOME/scripts:$HOME/.cargo/bin:$PATH"
+          # path
+          export PATH="$HOME/scripts:$HOME/.cargo/bin:$PATH"
 
-          			# pyenv
-          			export PYENV_ROOT="$HOME/.pyenv"
-          			[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-          			eval "$(pyenv init -)"
+          # pyenv
+          export PYENV_ROOT="$HOME/.pyenv"
+          [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+          eval "$(pyenv init -)"
 
-          			# GHCup environment
-          			[ -f "/home/sky/.ghcup/env" ] && . "/home/sky/.ghcup/env"
+          # GHCup environment
+          [ -f "/home/sky/.ghcup/env" ] && . "/home/sky/.ghcup/env"
 
-          			# opam 
-          			[[ ! -r '/home/sky/.opam/opam-init/init.zsh' ]] || source '/home/sky/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+          # opam 
+          [[ ! -r '/home/sky/.opam/opam-init/init.zsh' ]] || source '/home/sky/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
         '';
         configs = lib.mkDefault ''
-          			# Zoxide initialization
-          			eval "$(zoxide init --cmd cd zsh)"
+          # Zoxide initialization
+          eval "$(zoxide init --cmd cd zsh)"
 
-          			# WezTerm shell integration
-          			if [[ "$TERM_PROGRAM" == "WezTerm" ]]; then
-          			  source ~/.config/wezterm/shell-integration/wezterm.sh
-          			fi
+          # WezTerm shell integration
+          if [[ "$TERM_PROGRAM" == "WezTerm" ]]; then
+          source ~/.config/wezterm/shell-integration/wezterm.sh
+          fi
         '';
         overrides = lib.mkAfter ''
-          			if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="white"; fi
+          if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="white"; fi
 
-          			PROMPT='%{$fg[$NCOLOR]%}%B%n@%m%b%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} $(git_prompt_info)%(!.#.$) '
-          			RPROMPT='[%*]'
+          PROMPT='%{$fg[$NCOLOR]%}%B%n@%m%b%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} $(git_prompt_info)%(!.#.$) '
+          RPROMPT='[%*]'
 
-          			# Add vi mode indicator to prompt
-          			PROMPT="$(vi_mode_prompt_info)$PROMPT"
+          # Add vi mode indicator to prompt
+          PROMPT="\$(vi_mode_prompt_info)$PROMPT"
         '';
       in
       lib.mkMerge [ enviornments configs overrides ];
