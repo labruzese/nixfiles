92c8801d38d26090505f3eda23f59d2fc4208e48
Skylar Abruzese <skylar@abruzese.com>
2025-11-25 01:07:50 -0500

    rust telescope extension

--- modules/neovim/plugins/telescope.lua
+++ modules/neovim/plugins/telescope.lua
@@ -2,36 +2,37 @@ local telescope = require("telescope")
 local actions = require("telescope.actions")
 
 telescope.setup({
-    defaults = {
-        mappings = {
-            i = {
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                ["<esc>"] = actions.close,
-            },
-        },
-        file_ignore_patterns = {
-            "node_modules",
-            ".git",
-        },
-    },
-    pickers = {
-        find_files = {
-            hidden = true,
-        },
-    },
-    extensions = {
-        fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-        }
-    }
+	defaults = {
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<esc>"] = actions.close,
+			},
+		},
+		file_ignore_patterns = {
+			"node_modules",
+			".git",
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		}
+	}
 })
 
 telescope.load_extension("fzf")
+telescope.load_extension('rust')
 
 -- Mappings
 local map = vim.keymap.set
